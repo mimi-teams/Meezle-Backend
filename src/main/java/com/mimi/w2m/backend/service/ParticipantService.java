ef6ee1a297663c3d25823a0826a9dea88dab5ac3
YeongRoYun <colabofyyr@gmail.com>
2022-11-05 16:59:08 +0900

    Participant Service에 Event를 넣지 않았던 것 수정 ParticipantService의 createParticipant test case 추가 질의 : 현재 ParticipantService.createParticipant에서 중복된 이름의 Participant일 때, DuplicatedException에 발생하도록 만들었는데, 이러면 Mock을 사용해서 어떻게 테스트 하나용?

--- src/main/java/com/mimi/w2m/backend/service/ParticipantService.java
+++ src/main/java/com/mimi/w2m/backend/service/ParticipantService.java
@@ -2,12 +2,14 @@ package com.mimi.w2m.backend.service;
 
 import com.mimi.w2m.backend.domain.Participant;
 import com.mimi.w2m.backend.domain.type.Role;
+import com.mimi.w2m.backend.repository.EventRepository;
 import com.mimi.w2m.backend.repository.ParticipantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 /**
@@ -22,6 +24,7 @@ import java.util.NoSuchElementException;
 @Transactional(readOnly = true)
 public class ParticipantService {
     private final ParticipantRepository participantRepository;
+    private final EventRepository eventRepository;
 
     /**
      * 참여자 생성
@@ -30,13 +33,15 @@ public class ParticipantService {
      * @since 2022-11-01
      */
     @Transactional
-    public Long createParticipant(String name, String password) throws DuplicateKeyException {
+    public Long createParticipant(Long eventId, String name, String password) throws DuplicateKeyException, NoSuchElementException {
         participantRepository.findByName(name).ifPresent(entity -> {
             throw new DuplicateKeyException(name);
         });
+        var event = eventRepository.findById(eventId).orElseThrow();
         var participant = Participant.builder()
-                .name(name).
-                password(password)
+                .event(event)
+                .name(name)
+                .password(password)
                 .role(Role.PARTICIPANT)
                 .build();
         return participantRepository.save(participant).getId();
@@ -53,5 +58,8 @@ public class ParticipantService {
         return participant;
     }
 
-
+    public List<Participant> getAllParticipantInEvent(Long eventId) throws NoSuchElementException {
+        var event = eventRepository.findById(eventId).orElseThrow();
+        return participantRepository.findAllByEvent(event);
+    }
 }
