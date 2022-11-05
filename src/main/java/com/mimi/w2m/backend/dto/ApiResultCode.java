e0ea2d7218146b17101769f979601e53b7454e1a
yeh35 <sangohyeh@gmail.com>
2022-11-05 20:55:24 +0900

    fix : Swagger Full Name으로

--- src/main/java/com/mimi/w2m/backend/dto/ApiResultCode.java
+++ src/main/java/com/mimi/w2m/backend/dto/ApiResultCode.java
@@ -8,8 +8,8 @@ import static com.mimi.w2m.backend.dto.ApiHttpStatus.*;
 /**
  * API 응답 코드 규약
  *
- * @since 2022-11-05
  * @author yeh35
+ * @since 2022-11-05
  */
 
 public enum ApiResultCode {
@@ -19,11 +19,13 @@ public enum ApiResultCode {
     ENTITY_NOT_FOUND(3, NOT_FOUND, "해당 Entity 를 찾을 수 없음"),
     INVALID_VALUE(4, BAD_REQUEST, "잘못된 값으로 요청"),
 
+    EVENT_NOT_FOUND(5, NOT_FOUND, "이벤트가 존재하지 않습니다."),
     ;
 
     /**
      * 코드는 추가되는 순서대로 붙이자
      * 규칙성을 부여하면 좋겠지만 나중에 APP 서비스가 나와서 클라이언트를 수정하기 힘든 경우 고도화 하자
+     *
      * @since 2022-11-05
      */
     public final int code;
@@ -40,6 +42,6 @@ public enum ApiResultCode {
         return Arrays.stream(ApiResultCode.values())
                 .filter(apiResultCode -> apiResultCode.code == code)
                 .findFirst()
-                .orElseThrow(() ->  new EntityNotFoundException("잘못된 코드 입니다. code : " + code));
+                .orElseThrow(() -> new EntityNotFoundException("잘못된 코드 입니다. code : " + code));
     }
 }
--- src/main/java/com/mimi/w2m/backend/dto/event/EventDto.java
+++ src/main/java/com/mimi/w2m/backend/dto/event/EventDto.java
@@ -1,8 +1,10 @@
 package com.mimi.w2m.backend.dto.event;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Schema
 public class EventDto {
 
     private final Long id;
