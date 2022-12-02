cbf81b783829a8e36008f817a26cd9a673e81869
yeh35 <sangohyeh@gmail.com>
2022-12-02 19:35:06 +0900

    chor :  CodeStyle Formatting

--- src/main/java/com/mimi/w2m/backend/api/v1/UserApi.java
+++ src/main/java/com/mimi/w2m/backend/api/v1/UserApi.java
@@ -65,30 +65,28 @@ public class UserApi extends BaseGenericApi<UserService> {
         this.eventParticipantService = timeService;
     }
 
-    @Operation(method = "GET", summary = "이용자 정보 반환", description = "[로그인 O, 인가 X] ID에 해당하는 이용자 정보를 반환한다",
-               responses = {@ApiResponse(useReturnTypeSchema = true)}) @GetMapping(path = "/{id}")
+    @Operation(method = "GET",
+               summary = "이용자 정보 반환",
+               description = "[로그인 O, 인가 X] ID에 해당하는 이용자 정보를 반환한다",
+               responses = {@ApiResponse(useReturnTypeSchema = true)})
+    @GetMapping(path = "/{id}")
     public @Valid ApiCallResponse<UserResponseDto> get(
             @Parameter(name = "id", description = "이용자가 로그인할 때 제공된 ID", in = ParameterIn.PATH, required = true)
-            @PositiveOrZero @NotNull @Valid
-            @PathVariable("id")
-            Long id
+            @PositiveOrZero @NotNull @Valid @PathVariable("id") Long id
     ) {
         authService.getLoginInfo(httpSession);
         final var user = service.get(id);
         return ApiCallResponse.ofSuccess(UserResponseDto.of(user));
     }
 
-    @Operation(method = "PATCH", summary = "본인 정보 수정",
+    @Operation(method = "PATCH",
+               summary = "본인 정보 수정",
                description = "[로그인 O, 인가 O] ID에 해당하는 이용자의 정보를 수정한다. 현재 로그인한 이용자와 같아야 하며, 반환되는 정보는 없다",
-               responses = {@ApiResponse(useReturnTypeSchema = true)}) @PatchMapping(path = "/{id}")
+               responses = {@ApiResponse(useReturnTypeSchema = true)})
+    @PatchMapping(path = "/{id}")
     public @Valid ApiCallResponse<UserResponseDto> patch(
             @Parameter(name = "id", description = "이용자가 로그인할 때 제공된 ID", in = ParameterIn.PATH, required = true)
-            @PositiveOrZero @NotNull @Valid
-            @PathVariable("id")
-            Long id,
-            @Valid
-            @RequestBody
-            UserRequestDto requestDto
+            @PositiveOrZero @NotNull @Valid @PathVariable("id") Long id, @Valid @RequestBody UserRequestDto requestDto
     ) {
         final var loginInfo = authService.getLoginInfo(httpSession);
         authService.isValidLogin(loginInfo, id, Role.USER);
@@ -96,16 +94,16 @@ public class UserApi extends BaseGenericApi<UserService> {
         return ApiCallResponse.ofSuccess(null);
     }
 
-    @Operation(method = "DELETE", summary = "회원 탈퇴",
+    @Operation(method = "DELETE",
+               summary = "회원 탈퇴",
                description = "[로그인 O, 인가 O] ID에 해당하는 이용자를 삭제한다. 현재 로그인한 이용자와 같아야 하며, 삭제 시 Host 로 있던 Event 가 모두 삭제된다. " +
-                             "삭제 후, Logout 되고 '/' 로 Redirect 된다", responses = {
-            @ApiResponse(description = "'/'로 Redirect",
-                         content = {@Content(schema = @Schema(description = "GET '/'"))})})
-    @DeleteMapping(path = "/{id}") public ResponseEntity<?> delete(
+                             "삭제 후, Logout 되고 '/' 로 Redirect 된다",
+               responses = {@ApiResponse(description = "'/'로 Redirect",
+                                         content = {@Content(schema = @Schema(description = "GET '/'"))})})
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> delete(
             @Parameter(name = "id", description = "이용자가 로그인할 때 제공된 ID", in = ParameterIn.PATH, required = true)
-            @PositiveOrZero @NotNull @Valid
-            @PathVariable("id")
-            Long id
+            @PositiveOrZero @NotNull @Valid @PathVariable("id") Long id
     ) {
         final var loginInfo = authService.getLoginInfo(httpSession);
         authService.isValidLogin(loginInfo, id, Role.USER);
@@ -124,30 +122,34 @@ public class UserApi extends BaseGenericApi<UserService> {
         return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
     }
 
-    @Operation(method = "GET", summary = "이메일에 해당하는 이용자 정보 반환", description = "[로그인 O, 인가 X] Email에 해당하는 이용자를 반환한다",
-               responses = {@ApiResponse(useReturnTypeSchema = true)}) @GetMapping(path = "")
+    @Operation(method = "GET",
+               summary = "이메일에 해당하는 이용자 정보 반환",
+               description = "[로그인 O, 인가 X] Email에 해당하는 이용자를 반환한다",
+               responses = {@ApiResponse(useReturnTypeSchema = true)})
+    @GetMapping(path = "")
     public @Valid ApiCallResponse<UserResponseDto> getByEmail(
-            @Parameter(name = "email", description = "검색에 사용하는 email", in = ParameterIn.QUERY, required = true,
-                       schema = @Schema(pattern = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$"))
-            @Email @NotNull @Valid
-            @RequestParam
-            String email
+            @Parameter(name = "email",
+                       description = "검색에 사용하는 email",
+                       in = ParameterIn.QUERY,
+                       required = true,
+                       schema = @Schema(pattern = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")) @Email @NotNull @Valid
+            @RequestParam String email
     ) {
         final var loginInfo = authService.getLoginInfo(httpSession);
         final var user      = service.getByEmail(email);
         return ApiCallResponse.ofSuccess(UserResponseDto.of(user));
     }
 
-    @Operation(method = "GET", summary = "플랫폼을 이용한 로그인",
+    @Operation(method = "GET",
+               summary = "플랫폼을 이용한 로그인",
                description = "[로그인 X] Google 이나 Kakao 계정을 이용해 로그인한다. 로그인된 이용자가 있다면 로그아웃을 수행한다. 가입된 Email 이 없는 경우, " +
-                             "이용자를 새로 등록한다. 로그인 후, '/'로 Redirect 된다", responses = {
-            @ApiResponse(description = "'/'로 Redirect",
-                         content = {@Content(schema = @Schema(description = "GET '/'"))})}) @GetMapping(path = "/login")
+                             "이용자를 새로 등록한다. 로그인 후, '/'로 Redirect 된다",
+               responses = {@ApiResponse(description = "'/'로 Redirect",
+                                         content = {@Content(schema = @Schema(description = "GET '/'"))})})
+    @GetMapping(path = "/login")
     public ResponseEntity<?> loginWithOauth2(
             @Parameter(name = "platform", description = "로그인할 계정의 플랫폼", in = ParameterIn.QUERY, required = true)
-            @NotNull @Pattern(regexp = "^(kakao|google)$") @Valid
-            @RequestParam
-            String platform
+            @NotNull @Pattern(regexp = "^(kakao|google)$") @Valid @RequestParam String platform
     ) {
         authService.logout(httpSession);
         final var headers = new HttpHeaders();
@@ -155,15 +157,16 @@ public class UserApi extends BaseGenericApi<UserService> {
         return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
     }
 
-    @Operation(method = "GET", summary = "가입한 이용자의 로그아웃",
-               description = "[로그인 O, 인가 X] 이용자의 로그아웃을 처리한다. 로그아웃 후, '/'로 Redirect 된다", responses = {
-            @ApiResponse(description = "'/'로 Redirect",
-                         content = {@Content(schema = @Schema(description = "GET '/'"))})})
+    @Operation(method = "GET",
+               summary = "가입한 이용자의 로그아웃",
+               description = "[로그인 O, 인가 X] 이용자의 로그아웃을 처리한다. 로그아웃 후, '/'로 Redirect 된다",
+               responses = {@ApiResponse(description = "'/'로 Redirect",
+                                         content = {@Content(schema = @Schema(description = "GET '/'"))})})
     @GetMapping(path = "/logout")
     public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
         final var loginInfo = authService.getLoginInfo(httpSession);
-        final var auth      = SecurityContextHolder.getContext()
-                                                   .getAuthentication();
+        final var auth = SecurityContextHolder.getContext()
+                                              .getAuthentication();
         if(Objects.nonNull(auth)) {
             authService.logout(httpSession);
             new SecurityContextLogoutHandler().logout(request, response, auth);
