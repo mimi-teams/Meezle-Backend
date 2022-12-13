694d1a6374355e2f45b541f6149af6d10d4a2a1f
yeh35 <sangohyeh@gmail.com>
2022-12-13 20:07:13 +0900

    java-test-fixtures 적용

---
+++ src/testFixtures/java/com/mimi/w2m/backend/domain/UserTestFixture.java
@@ -0,0 +1,11 @@
+package com.mimi.w2m.backend.domain;
+
+public class UserTestFixture {
+
+    public static User createUser() {
+        return User.builder()
+
+                .build();
+    }
+
+}
