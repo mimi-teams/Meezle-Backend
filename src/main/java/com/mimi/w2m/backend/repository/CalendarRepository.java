b1256a644f5324e44677b3756085e7a030e70716
YeongRoYun <colabofyyr@gmail.com>
2023-01-12 20:08:07 +0900

    [#81] - 캘린더 이벤트 조회

--- src/main/java/com/mimi/w2m/backend/repository/CalendarRepository.java
+++ src/main/java/com/mimi/w2m/backend/repository/CalendarRepository.java
@@ -5,6 +5,7 @@ import com.mimi.w2m.backend.domain.Event;
 import com.mimi.w2m.backend.domain.User;
 import com.mimi.w2m.backend.domain.type.PlatformType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -20,9 +21,13 @@ import java.util.UUID;
  * @since 2023/01/11
  **/
 public interface CalendarRepository extends JpaRepository<Calendar, UUID> {
-    @Query(value = "select calendar from Calendar calendar where calendar.user = :user and calendar.event = :event")
-    Optional<Calendar> findByUserAndEvent(@Param("user") User user, @Param("event") Event event);
+    @Query(value = "select calendar from Calendar calendar where calendar.user = :user and calendar.event = :event and calendar.platform = :platform")
+    Optional<Calendar> findByUserAndEventInPlatform(@Param("user") User user, @Param("event") Event event, @Param("platform") PlatformType platform);
 
     @Query(value = "select calendar from Calendar calendar where calendar.user = :user and calendar.platform = :platform and calendar.platformCalendarId = :calendarId")
     List<Calendar> findAllByUserInPlatformCalendar(@Param("user") User user, @Param("platform") PlatformType platform, @Param("calendarId") String calendarId);
+
+    @Modifying
+    @Query(value = "delete from Calendar calendar where calendar.event = :event")
+    void deleteByEvent(@Param("event") Event event);
 }
