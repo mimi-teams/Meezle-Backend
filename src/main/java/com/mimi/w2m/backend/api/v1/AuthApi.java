f823f773621b7c8787750fc8118c98ade6bbc7a8
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-02-26 21:30:30 +0900

    add cloud logging

--- src/main/java/com/mimi/w2m/backend/api/v1/AuthApi.java
+++ src/main/java/com/mimi/w2m/backend/api/v1/AuthApi.java
@@ -18,6 +18,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,15 +37,18 @@ public class AuthApi {
     private final Oauth2Service oauth2Service;
     private final JwtHandler jwtHandler;
     private final Logger logger = LoggerFactory.getLogger("AuthApi");
+    @Value("${oauth2.kakao.redirect-url}")
+    private final String redirectUrl;
 
     /**
      * Kakao Oauth2 과정에서 인가 과정에서 사용된 Redirect URL 과 Access Token 을 발급할 때 사용된 것은 동일해야 한다.
      * 또한, 이 Redirect URL 은 KaKao App 의 Redirect Url 에 등록되어 있어야 한다.
      * + 캘린더 및 메시지 권한을 획득할 수 있도록 추가
+     * + Redirect URL = HTTPS인 경우, KAKAO에서 HTTP로 요청이 오기 때문에 Redirect URL이 불일치한다. 따라서 request.getURL이 아니라 직접 URL을 찾아 넘긴다.
+     *
      * @author teddy
      * @since 2023/01/07
-     *
-    **/
+     **/
     @Operation(summary = "KaKao OAuth2 이용한 로그인(1단계)",
             description = "KaKao Oauth2 를 이용한 로그인 단계 1. 요청 후, Kakao 계정으로 로그인할 수 있는 URL 가 반환된다. GET {authorizationUrl} 으로 2단계에 진입할 수 있다.",
             responses = {@ApiResponse(description = "'/'로 Redirect",
@@ -60,6 +64,7 @@ public class AuthApi {
                 .build();
         return ApiCallResponse.ofSuccess(response);
     }
+
     @Operation(summary = "KaKao OAuth2 이용한 로그인(2단계)",
             description = "1단계에서 GET {authorizationUrl} 이후 자동으로 Redirect 되며, Jwt Token 이 발급된다.")
     @GetMapping(path = "/oauth2/authorization/redirect/kakao")
@@ -67,8 +72,9 @@ public class AuthApi {
             HttpServletRequest request,
             @RequestParam String code
     ) {
-        logger.info(request.getRequestURL().toString());
-        final User user = oauth2Service.afterAuthorization(PlatformType.KAKAO, code, request.getRequestURL().toString());
+//        logger.info(this.redirectUrl);
+
+        final User user = oauth2Service.afterAuthorization(PlatformType.KAKAO, code, this.redirectUrl);
         final String token = jwtHandler.createToken(user.getId(), Role.USER);
 
         return ApiCallResponse.ofSuccess(
