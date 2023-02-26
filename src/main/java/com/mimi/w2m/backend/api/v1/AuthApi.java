b55ab884e6165d3bf67a569204a40f57176452a5
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-02-26 23:45:41 +0900

    100 bugfix kakao redirect url (#104)

--- src/main/java/com/mimi/w2m/backend/api/v1/AuthApi.java
+++ src/main/java/com/mimi/w2m/backend/api/v1/AuthApi.java
@@ -18,6 +18,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,11 +27,13 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 @Tag(name = "Auth Api", description = "로그인/회원가입")
 @RequiredArgsConstructor
 @RequestMapping(path = "/v1/auth")
 @RestController
+@Validated
 public class AuthApi {
 
     private final Oauth2Service oauth2Service;
@@ -54,7 +57,7 @@ public class AuthApi {
             responses = {@ApiResponse(description = "'/'로 Redirect",
                     content = {@Content(schema = @Schema(description = "GET '/'"))})})
     @GetMapping(path = "/oauth2/authorization")
-    public ApiCallResponse<OAauth2AuthorizationResponse> oauth2Authorization(
+    public @Valid ApiCallResponse<OAauth2AuthorizationResponse> oauth2Authorization(
             @Parameter(name = "platform", description = "로그인할 계정의 플랫폼", in = ParameterIn.QUERY, required = true)
             @Valid @NotNull @RequestParam PlatformType platform
     ) {
@@ -68,13 +71,16 @@ public class AuthApi {
     @Operation(summary = "KaKao OAuth2 이용한 로그인(2단계)",
             description = "1단계에서 GET {authorizationUrl} 이후 자동으로 Redirect 되며, Jwt Token 이 발급된다.")
     @GetMapping(path = "/oauth2/authorization/redirect/kakao")
-    public ApiCallResponse<LoginSuccessResponse> oauth2Authorization(
+    public @Valid ApiCallResponse<LoginSuccessResponse> oauth2Authorization(
             HttpServletRequest request,
-            @RequestParam String code
+            @Valid @NotNull @RequestParam String code,
+            @RequestParam String redirectUrl
     ) {
-        var redirectUrl = request.getRequestURL().toString();
-        if (!redirectUrl.contains("localhost")) {
-            redirectUrl = redirectUrl.replace("http", "https");
+        if(Objects.isNull(redirectUrl)) {
+            redirectUrl = request.getRequestURL().toString();
+            if (!redirectUrl.contains("localhost")) {
+                redirectUrl = redirectUrl.replace("http", "https");
+            }
         }
         logger.info(redirectUrl);
         final User user = oauth2Service.afterAuthorization(PlatformType.KAKAO, code, redirectUrl);
